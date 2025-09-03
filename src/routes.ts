288b94f5e67cfd534941203cba9fa57395cc8158
Alexei <avenge.now.r@mail.ru>
2025-09-03 22:15:09 +0300

    feat: add paths to routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,3 +1,16 @@
-import { type RouteConfig, index } from '@react-router/dev/routes';
+import {
+  type RouteConfig,
+  index,
+  layout,
+  route,
+} from '@react-router/dev/routes';
 
-export default [index('routes/home.tsx')] satisfies RouteConfig;
+export default [
+  layout('./layout.tsx', [
+    index('./routes/main/index.tsx'),
+    route('auth', './routes/auth/index.tsx'),
+    route('rest', './routes/rest/index.tsx'),
+    route('variables', './routes/variables/index.tsx'),
+    route('history', './routes/history/index.tsx'),
+  ]),
+] satisfies RouteConfig;
