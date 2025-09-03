6437a138ef343a41df604c8c442a73de5baa3683
Andrey Poleshchuk <a.paleshchuk@brimit.com>
2025-09-04 01:00:59 +0300

    feat: add signup

--- src/firebase.ts
+++ src/firebase.ts
@@ -21,29 +21,22 @@ const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
 
-const logInWithEmailAndPassword = async (email, password) => {
-  try {
-    await signInWithEmailAndPassword(auth, email, password);
-  } catch (err) {
-    console.error(err);
-    alert(err.message);
-  }
+const logInWithEmailAndPassword = async (email: string, password: string) => {
+  await signInWithEmailAndPassword(auth, email, password);
 };
 
-const registerWithEmailAndPassword = async (name, email, password) => {
-  try {
-    const res = await createUserWithEmailAndPassword(auth, email, password);
-    const user = res.user;
-    await addDoc(collection(db, 'users'), {
-      uid: user.uid,
-      name,
-      authProvider: 'local',
-      email,
-    });
-  } catch (err) {
-    console.error(err);
-    alert(err.message);
-  }
+const registerWithEmailAndPassword = async (
+  email: string,
+  password: string
+) => {
+  const res = await createUserWithEmailAndPassword(auth, email, password);
+  const user = res.user;
+  await addDoc(collection(db, 'users'), {
+    uid: user.uid,
+    authProvider: 'local',
+    email,
+  });
+  return user;
 };
 
 const logout = () => {
