ceeb14c8a841f34e813d47ee9a93abf7ba600b28
Andrey Poleshchuk <a.paleshchuk@brimit.com>
2025-09-05 00:27:06 +0300

    feat: add signin and protected route

--- src/routes.ts
+++ src/routes.ts
@@ -10,8 +10,10 @@ export default [
     index('./routes/main/index.tsx'),
     route('signup', 'routes/signup/index.tsx'),
     route('signin', 'routes/signin/index.tsx'),
-    route('rest', './routes/rest/index.tsx'),
-    route('variables', './routes/variables/index.tsx'),
-    route('history', './routes/history/index.tsx'),
+    layout('./routes/PrivateRoute.tsx', [
+      route('rest', './routes/rest/index.tsx'),
+      route('variables', './routes/variables/index.tsx'),
+      route('history', './routes/history/index.tsx'),
+    ]),
   ]),
 ] satisfies RouteConfig;
--- src/routes/signup/__tests__/index.test.tsx
+++ src/routes/signup/__tests__/index.test.tsx
@@ -1,11 +1,16 @@
 import { expect, test } from 'vitest';
 import { render, screen } from '@testing-library/react';
 import SignUp from '..';
+import { MemoryRouter } from 'react-router';
 
 test('SignUp renders', async () => {
   const TITLE = 'Sign Up';
 
-  render(<SignUp />);
+  render(
+    <MemoryRouter>
+      <SignUp />
+    </MemoryRouter>
+  );
 
   expect(screen.getByText(TITLE)).toBeInTheDocument();
 });
