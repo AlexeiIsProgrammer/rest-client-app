d1233dcbd5c4dff512318abd89ff966bb1034e15
Andrey Poleshchuk <a.paleshchuk@brimit.com>
2025-09-05 00:27:06 +0300

    feat: add signin and protected route

--- src/routes/signin/__tests__/index.test.tsx
+++ src/routes/signin/__tests__/index.test.tsx
@@ -1,11 +1,16 @@
 import { expect, test } from 'vitest';
 import { render, screen } from '@testing-library/react';
 import SignIn from '..';
+import { MemoryRouter } from 'react-router';
 
 test('SignIn renders', async () => {
   const TITLE = 'Sign In';
 
-  render(<SignIn />);
+  render(
+    <MemoryRouter>
+      <SignIn />
+    </MemoryRouter>
+  );
 
   expect(screen.getByText(TITLE)).toBeInTheDocument();
 });
