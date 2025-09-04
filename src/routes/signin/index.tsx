c631a0979f14de1bf26c2437b056a5b1340525dd
Andrey Poleshchuk <a.paleshchuk@brimit.com>
2025-09-04 22:02:53 +0300

    feat: add signin

---
+++ src/routes/signin/index.tsx
@@ -0,0 +1,59 @@
+import { useState } from 'react';
+import { Container, Typography } from '@mui/material';
+import Spinner from '../../components/Spinner/Spinner';
+import AuthForm from '../../components/AuthForm/AuthForm';
+import { logInWithEmailAndPassword } from '../../firebase';
+import { validateEmail } from '../../utils/validation';
+import { useNavigate, Link } from 'react-router';
+
+export default function SignIn() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState<string | null>(null);
+  const navigate = useNavigate();
+
+  async function handleSubmit(e: React.FormEvent) {
+    e.preventDefault();
+    if (!validateEmail(email)) {
+      return setError('Invalid email format');
+    }
+
+    try {
+      setIsLoading(true);
+      await logInWithEmailAndPassword(email, password);
+      navigate('/rest');
+    } catch (err) {
+      if (err instanceof Error) {
+        setError(err.message);
+      } else {
+        setError('An unexpected error occurred');
+      }
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  return (
+    <>
+      {isLoading && <Spinner />}
+      <Container maxWidth="sm">
+        <Typography variant="h4" gutterBottom>
+          Sign In
+        </Typography>
+        <AuthForm
+          email={email}
+          setEmail={setEmail}
+          password={password}
+          setPassword={setPassword}
+          handleSubmit={handleSubmit}
+          error={error}
+          buttonText="Login"
+        />
+        <Typography sx={{ mt: 2, textAlign: 'center' }}>
+          <Link to="/signup">Sign up</Link>
+        </Typography>
+      </Container>
+    </>
+  );
+}
