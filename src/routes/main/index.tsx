4833c563b4a243e8c1b8ec5e8e37fca6176dbb2b
Andrey Poleshchuk <a.paleshchuk@brimit.com>
2025-09-17 22:10:09 +0300

    feat: add tests for history page

--- src/routes/main/index.tsx
+++ src/routes/main/index.tsx
@@ -3,12 +3,14 @@ import { getUserFromRequest } from '../../utils/auth.server';
 import MainNonAuthorized from '../../components/Main/MainNonAuthorized';
 import MainAuthorized from '../../components/Main/MainAuthorized';
 
+/* v8 ignore start -- @preserve */
 export function meta() {
   return [
     { title: 'Main page' },
     { name: 'description', content: 'Welcome to Main page!' },
   ];
 }
+/* v8 ignore stop -- @preserve */
 
 export async function loader({ request }: { request: Request }) {
   const user = await getUserFromRequest(request);
